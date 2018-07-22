b8c20863a3c021db69aecbeb78936cbc3b919c3c
Joe Loser <joeloser93@gmail.com>
2018-07-22 12:09:38 -0400

    Use ripgrep for FZF_DEFAULT_COMMAND
    Problem:
    - Using fd is not ideal for FZF_DEFAULT_COMMAND.
    - Not redirecting errors to /dev/null.
    
    Solution:
    - Use ripgrep.
    - Redirect errors to /dev/null.

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,4 +1,5 @@
 export FZF_DEFAULT_OPS="--extended --ansi"
-export FZF_DEFAULT_COMMAND='fd --type file --color=always --hidden --follow --exclude .git'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --color=always --hidden --follow --exclude .git 2> /dev/null'
 export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
 export FZF_COMPLETION_OPTS='+c -x'
+

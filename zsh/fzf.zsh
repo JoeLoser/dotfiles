1895366f3195040a34170481d11fa89ee4c03ac8
Joe Loser <joeloser93@gmail.com>
2019-03-04 14:37:26 -0500

    [zsh] use fd instead of rg for FZF_DEFAULT_COMMAND

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,5 +1,6 @@
 export FZF_DEFAULT_OPS="--extended --ansi"
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --color=always --hidden --follow --exclude .git 2> /dev/null'
+#export FZF_DEFAULT_COMMAND='rg --files --no-ignore --color=always --hidden --follow --exclude .git 2> /dev/null'
+export FZF_DEFAULT_COMMAND='fd --type file --no-ignore --color=always --hidden --follow --exclude .git 2> /dev/null'
 export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
 export FZF_COMPLETION_OPTS='+c -x'
 

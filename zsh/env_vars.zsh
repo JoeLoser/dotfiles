b82d76d1e540f80cd7d9e4a412e3b3faa698893b
Joe Loser <joe@modular.com>
2022-08-31 16:56:21 -0600

    [zsh] Set GTEST_COLOR and CTEST_OUTPUT_ON_FAILURE environment vars

--- zsh/env_vars.zsh
+++ zsh/env_vars.zsh
@@ -3,3 +3,6 @@ export EDITOR='vim'
 export TZ=/usr/share/zoneinfo/US/Mountain
 export SSH_KEY_PATH="~/.ssh/rsa_id"
 export BROWSER=google-chrome
+
+export GTEST_COLOR=1
+export CTEST_OUTPUT_ON_FAILURE=1

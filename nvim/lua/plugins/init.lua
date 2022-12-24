6bcc65c45c28f4e6e740dedb6c621e61784723b5
Joe Loser <joeloser@fastmail.com>
2022-12-23 21:51:20 -0700

    [nvim] More robust way of checking packer installed already

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,10 +1,31 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print('Installing packer. Please close and reopen Neovim.')
-  vim.cmd [[packadd packer.nvim]]
+
+-- The root dir to install all plugins. Plugins are under opt/ or start/ sub-directory.
+vim.g.plugin_home = fn.stdpath("data") .. "/site/pack/packer"
+
+--- Install packer if it has not been installed.
+--- Return:
+--- true: if this is a fresh install of packer
+--- false: if packer has been installed
+local function packer_ensure_install()
+  -- Where to install packer.nvim -- the package manager (we make it opt)
+  local packer_dir = vim.g.plugin_home .. "/opt/packer.nvim"
+
+  if fn.glob(packer_dir) ~= "" then
+    return false
+  end
+
+  -- Auto-install packer in case it hasn't been installed.
+  vim.api.nvim_echo({ { "Installing packer.nvim", "Type" } }, true, {})
+
+  local packer_repo = "https://github.com/wbthomason/packer.nvim"
+  local install_cmd = string.format("!git clone --depth=1 %s %s", packer_repo, packer_dir)
+  vim.cmd(install_cmd)
+
+  return true
 end
+
+local fresh_install = packer_ensure_install()
 
 -- Automatically run :PackerCompile whenever plugins.lua is updated
 --vim.cmd([[

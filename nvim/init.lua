832dd9e37fd3d4d24f537d52e6bf2124e789dba5
Joe Loser <joeloser@fastmail.com>
2022-12-25 15:41:25 -0700

    [nvim] Disable netrw at start
    This is recommended by `nvim-tree` plugin.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 require('core')
 require('core.options')
 require('core.mappings')
+require('core.globals')
 
 require('plugins')
 
---
+++ nvim/lua/core/globals.lua
@@ -0,0 +1,8 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- Do not load the tutor plugin
+vim.g.loaded_tutor_mode_plugin = 1

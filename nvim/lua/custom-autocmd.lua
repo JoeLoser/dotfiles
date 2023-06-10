ce8d1a07ef1f64ed88f47ac6c0b436a6419e7a19
Joe Loser <joeloser@fastmail.com>
2023-06-09 21:28:40 -0600

    [nvim][wip] Add autocommands and custom-autocmd

---
+++ nvim/lua/custom-autocmd.lua
@@ -0,0 +1,6 @@
+-- Resize all windows when we resize the terminal
+api.nvim_create_autocmd("VimResized", {
+  group = api.nvim_create_augroup("win_autoresize", { clear = true }),
+  desc = "autoresize windows on resizing operation",
+  command = "wincmd =",
+})
